d14ff19dca2e93d1e2d5cd30bdee882ebc8f7ddc
AcilRestu12 <arieskarestu214@gmail.com>
2021-11-21 15:57:50 +0700

    Object still can't detected

--- Code/Main.py
+++ Code/Main.py
@@ -13,53 +13,59 @@ from PIL import Image, ImageTk
 colorObject =  [                               # h_min, s_min, v_min, h_max, s_max, v_max
             [61, 48, 25, 179, 255, 185],
             [94, 98, 0, 138, 149, 40],
-            [111, 87, 0, 179, 255, 255]        # pen biru
+            [111, 87, 0, 179, 255, 255],        # pen biru
+            [86, 74, 0, 179, 74, 132]
             ]     
 
 colorPoint = [                           # BGR
                 [51,153,255],
                 [237, 202, 147],
+                [116, 113, 244],
                 [116, 113, 244]
                 ]              
 
 points = []                               # x, y, colorId
 
 
-# def findColor(img, colorObject, colorPoint):
-#     imgHSV = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-#     count = 0
-#     newPoints = []
-#     for color in colorObject:
-#         lower = np.array(color[0:3])
-#         upper = np.array(color[3:6])
-#         mask = cv.inRange(imgHSV, lower, upper)
-#         x, y = getContours(mask)
+def findColor(img, colorObject, colorPoint):
+    global imgResult
+    
+    imgHSV = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    count = 0
+    newPoints = []
+    for color in colorObject:
+        lower = np.array(color[0:3])
+        upper = np.array(color[3:6])
+        mask = cv.inRange(imgHSV, lower, upper)
+        x, y = getContours(mask)
 
-#         cv.circle(imgResult, (x,y), 10, colorPoint[count], cv.FILLED)
-#         if x != 0 & y != 0:
-#             newPoints.append([x,y,count])
-#         # newPoints.append([x,y,count])
-#         count += 1
-#         # cv.imshow(str(color[0]), mask)
-#     return newPoints
-
-
-# def getContours(img):
-#     contours, hierarchy = cv.findContours(img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
-#     x,y,w,h = 0,0,0,0
-#     for cnt in contours:
-#         area = cv.contourArea(cnt)
-#         if area > 500:
-#             # cv.drawContours(imgResult, cnt, -1, (255, 0, 0), 3)
-#             peri = cv.arcLength(cnt, True)
-#             approx = cv.approxPolyDP(cnt, 0.02 * peri, True)
-#             x, y, w, h = cv.boundingRect(approx)
-#     return x+w // 2, y
+        cv.circle(imgResult, (x,y), 10, colorPoint[count], cv.FILLED)
+        if x != 0 & y != 0:
+            newPoints.append([x,y,count])
+        newPoints.append([x,y,count])
+        count += 1
+        # cv.imshow(str(color[0]), mask)
+    return newPoints
 
 
-# def drawOnCanvas(points, colorPoint):
-#     for point in points:
-#         cv.circle(imgResult, (point[0], point[1]), 10, colorPoint[point[2]], cv.FILLED)
+def getContours(img):
+    contours, hierarchy = cv.findContours(img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+    x,y,w,h = 0,0,0,0
+    for cnt in contours:
+        area = cv.contourArea(cnt)
+        if area > 500:
+            # cv.drawContours(imgResult, cnt, -1, (255, 0, 0), 3)
+            peri = cv.arcLength(cnt, True)
+            approx = cv.approxPolyDP(cnt, 0.02 * peri, True)
+            x, y, w, h = cv.boundingRect(approx)
+    return x+w // 2, y
+
+
+def drawOnCanvas(points, colorPoint):
+    global imgResult
+    
+    for point in points:
+        cv.circle(imgResult, (point[0], point[1]), 10, colorPoint[point[2]], cv.FILLED)
 
 
 # def main():
@@ -98,12 +104,24 @@ def resizeImg(img, width, height):
 
 
 def videoStream():
-    global cap
+    global cap, imgResult
+    
     sucess, img = cap.read()
     imgCV = cv.cvtColor(img, cv.COLOR_BGR2RGBA)
     imgCV = resizeImg(imgCV, 852, 480)
     imgPill = Image.fromarray(imgCV)
     imgtk = ImageTk.PhotoImage(image=imgPill)
+    
+    
+    imgResult = img.copy()
+    newPoints = findColor(img, colorObject, colorPoint)
+    if len(newPoints) != 0:
+        for newP in newPoints:              # Kita tidak bisa menaruh list ke list.
+            print(newP)
+            points.append(newP)
+    if len(points) != 0:
+        drawOnCanvas(points, colorPoint)
+    
     lblOriImg.imgtk = imgtk
     lblOriImg.configure(image=imgtk)
     lblOriImg.pack()
